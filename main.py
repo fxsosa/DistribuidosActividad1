c182872caa7bf90474eb9d56231d9f2edb63e718
Leo Pedrozo <43396193+LeoPedrozo@users.noreply.github.com>
2021-07-22 16:00:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+from datetime import datetime
+
 print("buenas")
 print("noches")
 
+
+now = datetime.now()
+
+current_time = now.strftime("%H:%M:%S")
+print("Codigo modificado por Leo,la hora es ", current_time)
